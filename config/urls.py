b15a47f0044761fe5a744fecd56f44c9cfd05c44
GJ-Nick <js.developer.12@gmail.com>
2016-02-23 14:37:11 +0200

    Remove templates

--- config/urls.py
+++ config/urls.py
@@ -5,27 +5,10 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.views.generic import TemplateView
-from django.views import defaults as default_views
-urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='pages/home.html'), name="home"),
-    url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name="about"),
 
+urlpatterns = [
     # Django Admin, use {% url 'admin:index' %}
     url(settings.ADMIN_URL, include(admin.site.urls)),
-    url(r'^users/', include("citifleet.users.urls", namespace="users")),
-
-    # Your stuff: custom urls includes go here
-
-
+    url(r'^users/', include('citifleet.users.urls', namespace='users')),
+    url(r'^reports/', include('citifleet.reports.urls', namespace='reports')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-if settings.DEBUG:
-    # This allows the error pages to be debugged during development, just visit
-    # these url in browser to see how these error pages look like.
-    urlpatterns += [
-        url(r'^400/$', default_views.bad_request),
-        url(r'^403/$', default_views.permission_denied),
-        url(r'^404/$', default_views.page_not_found),
-        url(r'^500/$', default_views.server_error),
-    ]
