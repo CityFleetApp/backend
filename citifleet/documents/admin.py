3de552bde35f577600ddc53979444c47af21047d
Nick Guzy <guzy@steelkiwi.com>
2016-03-14 14:16:02 +0000

    Register document in admin

--- citifleet/documents/admin.py
+++ citifleet/documents/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Document
+
+
+class DocumentModelAdmin(admin.ModelAdmin):
+    list_display = ('user', 'document_type', 'status', 'expired')
+    list_filter = ('status', 'document_type')
+    actions = ['make_declined', 'make_confirmed']
+
+    def make_declined(self, request, queryset):
+        queryset.update(status=Document.DECLINED)
+    make_declined.short_description = 'Mark selected documents as declined'
+
+    def make_confirmed(self, request, queryset):
+        queryset.update(status=Document.CONFIRMED)
+    make_confirmed.short_description = 'Mark selected documents as confirmed'
+
+admin.site.register(Document, DocumentModelAdmin)
