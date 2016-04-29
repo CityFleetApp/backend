6ed579b9f7a43361f767e6e7b89cf1d144798404
Nick Guzy <guzy@steelkiwi.com>
2016-04-29 18:10:14 +0300

    Add card to vault

--- citifleet/payments/views.py
+++ citifleet/payments/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.views.generic import FormView
 
-# Create your views here.
+from paypalrestsdk import CreditCard
+
+from .forms import RegisterCardForm
+
+
+class RegisterCardView(FormView):
+    form_class = RegisterCardForm
+
+    def form_valid(self, form):
+        credit_card = CreditCard({
+            "type": "visa",
+            "number": "4417119669820331",
+            "expire_month": "11",
+            "expire_year": "2018",
+            "cvv2": "874",
+            "first_name": "Joe",
+            "last_name": "Shopper"})
+
+        if credit_card.create() and credit_card['state'] == 'ok':
+            print('Save credit card %s' % credit_card.id)
