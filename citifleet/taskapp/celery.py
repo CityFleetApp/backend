949fc8341906dd57e5fee7ffdb348acf199e0dfa
Nikolay Gavryliuk <gavrilyuk.nikolay@gmail.com>
2016-09-11 20:09:20 +0300

    update celery.py file

--- citifleet/taskapp/celery.py
+++ citifleet/taskapp/celery.py
@@ -6,11 +6,11 @@ import os
 
 from celery import Celery
 
-from django.apps import apps
+from django.conf import settings
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 
 app = Celery('citifleet')
 app.config_from_object('django.conf:settings')
-app.autodiscover_tasks(lambda: [n.name for n in apps.get_app_configs()], force=True)
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS, force=True)
