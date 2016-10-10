6712bb795d3241cbcc692fda0aec2f88349a243d
Nikolay Gavryliuk <gavrilyuk.nikolay@gmail.com>
2016-10-10 22:53:24 +0300

    CIT-92 add logs for firebase

--- citifleet/fcm_notifications/admin.py
+++ citifleet/fcm_notifications/admin.py
@@ -3,8 +3,16 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
 from citifleet.fcm_notifications.models import FCMDevice
 
+User = get_user_model()
 
-admin.site.register(FCMDevice)
+
+@admin.register(FCMDevice)
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'device_id', 'user', 'active', 'date_created')
+    list_filter = ('active', )
+    raw_id_fields = ('user', )
+    search_fields = ('name', 'device_id', 'user__username')
--- citifleet/fcm_notifications/tasks.py
+++ citifleet/fcm_notifications/tasks.py
@@ -9,26 +9,42 @@ from citifleet.fcm_notifications.fcm import CustomFCMNotification
 def send_push_notification_task(api_key, registration_ids, kwargs):
     from citifleet.fcm_notifications.utils import update_fcmdevice_registration_id, remove_fcmdevice_registration_id
 
+    logger = send_push_notification_task.get_logger()
+
     push_service = CustomFCMNotification(api_key=api_key)
     if not isinstance(registration_ids, list):
         registration_ids = [registration_ids, ]
 
+    logger.debug('Send push notification to %s registration ids with kwargs: %s', len(registration_ids), kwargs)
+
     kwargs['registration_ids'] = registration_ids
     responses = push_service.notify_multiple_devices(**kwargs)
+    logger.debug('Receive %s responses', len(responses))
+
     for response in responses:
         if response['status_code'] != 200:
-            print('some error occurred. Status code: %s', response['status_code'])
+            logger.debug('Some error occurred. Status code: %s', response['status_code'])
             return
+
         if not response['response']['failure'] and not response['response']['canonical_ids']:
+            logger.debug('There are nothing to process. Successful response.')
             continue
 
-        registration_ids_to_resend = []  # TODO: probably add resend mechanism
+        registration_ids_to_resend = []
         if response['registration_ids']:
             for i, result in enumerate(response['response'].get('results', [])):
                 if result.get('registration_id'):
+                    logger.debug(
+                        'Update registration id from %s to %s',
+                        response['registration_ids'][i],
+                        result['registration_id']
+                    )
                     update_fcmdevice_registration_id(response['registration_ids'][i], result['registration_id'])
                 elif result.get('error'):
                     if result['error'] == 'Unavailable':
                         registration_ids_to_resend.append(response['registration_ids'][i])
                     else:
+                        logger.debug('Remove device with registration id %s', response['registration_ids'][i])
                         remove_fcmdevice_registration_id(response['registration_ids'][i])
+
+        logger.debug('Push notifications are successfully sent')
